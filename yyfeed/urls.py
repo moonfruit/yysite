52286a04c5cb155a849ab18e24dc59029e0b4288
MoonFruit <dkmoonfruit@gmail.com>
2016-12-16 10:13:32 +0800

    add favicon

--- yyfeed/urls.py
+++ yyfeed/urls.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage as storage
 
 from . import views
 from .views import AtomFeed, RssFeed
@@ -9,9 +11,13 @@ feed = RssFeed()
 
 app_name = 'yyfeed'
 urlpatterns = [
+    url(r'^favicon.ico$',
+        RedirectView.as_view(url=storage.url("/yyfeed/favicon.ico"), permanent=True),
+        name='favicon'
+        ),
     url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
-    url(r'^feed/(?P<name>\w+)/$', feed),
-    url(r'^feed/(?P<name>\w+)/rss/$', feed),
-    url(r'^feed/(?P<name>\w+)/atom/$', AtomFeed()),
+    url(r'^feed/(?P<name>\w+)/$', feed, name='feed'),
+    url(r'^feed/(?P<name>\w+)/rss/$', feed, name='feed'),
+    url(r'^feed/(?P<name>\w+)/atom/$', AtomFeed(), name='feed'),
 ]

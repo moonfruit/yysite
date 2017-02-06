32b3009446da6d0e9d477d0a8148cb71fcad7a88
MoonFruit <dkmoonfruit@gmail.com>
2017-02-06 11:10:58 +0800

    modify time for ttrss

--- yyfeed/cron/job.py
+++ yyfeed/cron/job.py
@@ -70,7 +70,7 @@ class TtrssJob(CronJobBase, FetcherJob):
 
 class RosiyyJob(CronJobBase, FetcherJob):
     RUN_EVERY_MINS = 59
-    RETRY_AFTER_FAILURE_MINS = 9
+    RETRY_AFTER_FAILURE_MINS = 719
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS,
                         retry_after_failure_mins=RETRY_AFTER_FAILURE_MINS)

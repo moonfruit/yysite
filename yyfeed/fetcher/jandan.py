b95cdbb65e8ec6b739d17fa62133ab8b96c5e296
MoonFruit <dkmoonfruit@gmail.com>
2017-12-08 11:09:35 +0800

    bug fix for jandan to get key

--- yyfeed/fetcher/jandan.py
+++ yyfeed/fetcher/jandan.py
@@ -1,43 +1,72 @@
 # -*- coding: utf-8 -*-
-from base64 import b64decode
-from hashlib import md5 as md5digest
-from re import sub
+import base64
+import hashlib
+import logging
+import re
 from typing import Iterable
 
+# noinspection PyProtectedMember
 from bs4 import SoupStrainer
 
 from .base import Fetcher, Item
+
+logger = logging.getLogger(__name__)
 
 
 class JandanFetcher(Fetcher):
     URL = 'http://jandan.net/ooxx'
     FILTER = SoupStrainer('ol', 'commentlist')
 
-    def __init__(self, key='Aw9P1Fjfo2fJ0cigUoM094bJ4g8FPzop'):
-        super().__init__()
-        self.key = key
-
     def fetch(self, count=5) -> Iterable[Item]:
         current = 1
+        key = None
         for i in range(count):
             if i == 0:
                 soup = self.fetcher.soup(self.URL)
+                key = self.get_key(soup)
+                logger.debug("Get key [%s]", key)
+
                 current = int(soup.find('span', 'current-comment-page').text[1: -1])
                 ol = soup.find(self.FILTER)
-                for item in self.generate(ol):
+                for item in self.generate(ol, key):
                     yield item
 
             else:
                 current -= 1
-                for item in self.fetch_page(current):
+                for item in self.fetch_page(current, key):
                     yield item
 
-    def fetch_page(self, page) -> Iterable[Item]:
+    def fetch_page(self, page, key) -> Iterable[Item]:
+        if not key:
+            raise RuntimeError("No key")
+
         url = '%s/page-%d' % (self.URL, page)
         ol = self.fetcher.soup(url, parse_only=self.FILTER)
-        return self.generate(ol)
+        return self.generate(ol, key)
 
-    def generate(self, ol) -> Iterable[Item]:
+    def get_key(self, soup) -> str:
+        scripts = soup.find('head').find_all('script')
+        key_url = None
+        for script in scripts:
+            src = script.get('src')
+            if src and re.match(r'//cdn.jandan.net/static/min/\w+\.\d+\.js', src, re.ASCII):
+                key_url = src
+        if not key_url:
+            raise RuntimeError("Cannot get key url")
+        key_url = normalize(key_url)
+
+        js = self.fetcher.fetch(key_url)
+        match = re.search(r'jandan_load_img\(.*\){.*f_\w+\(.*?"(.*?)"\).*}', js, re.ASCII)
+        key = None
+        if match:
+            key = match.group(1)
+        if not key:
+            raise RuntimeError("Cannot get key from [%s]" % key_url)
+
+        return key
+
+    @staticmethod
+    def generate(ol, key) -> Iterable[Item]:
         for item in ol.find_all('li'):
             if not item.get('id'):
                 continue
@@ -47,7 +76,7 @@ class JandanFetcher(Fetcher):
 
             imgs = []
             for img in text.find_all('span', 'img-hash'):
-                src = decode(img.text, self.key)
+                src = decode(img.text, key)
                 imgs.append('<div><img src="%s"/></div>' % src)
 
             if imgs:
@@ -57,14 +86,20 @@ class JandanFetcher(Fetcher):
 def md5(text):
     if isinstance(text, str):
         text = text.encode()
-    return md5digest(text).hexdigest()
+    return hashlib.md5(text).hexdigest()
 
 
 def base64decode(text):
     mod = len(text) % 4
     if mod != 0:
         text += '=' * (4 - mod)
-    return b64decode(text)
+    return base64.b64decode(text)
+
+
+def normalize(url):
+    if url.startswith('//'):
+        url = "http:" + url
+    return url
 
 
 def decode(cipher, key):
@@ -87,17 +122,20 @@ def decode(cipher, key):
         index = (index + array[i] + array2[i]) % 256
         array[i], array[index] = array[index], array[i]
 
-    results = []
     index = index2 = 0
-    for i in range(len(cipher_tail)):
+    for i in range(26):
         index = (index + 1) % 256
         index2 = (index2 + array[index]) % 256
         array[index], array[index2] = array[index2], array[index]
-        results.append(chr(cipher_tail[i] ^ (array[(array[index] + array[index2]) % 256])))
 
-    results = ''.join(results)[26:]
-    results = sub(r'(//\w+\.sinaimg\.cn/)(\w+)(/.+\.(gif|jpg|jpeg))', '\\1large\\3', results)
-    if results.startswith('//'):
-        results = "http:" + results
+    result = ''
+    for i in range(26, len(cipher_tail)):
+        index = (index + 1) % 256
+        index2 = (index2 + array[index]) % 256
+        array[index], array[index2] = array[index2], array[index]
+        result += chr(cipher_tail[i] ^ (array[(array[index] + array[index2]) % 256]))
 
-    return results
+    result = re.sub(r'(//\w+\.sinaimg\.cn/)(\w+)(/.+\.(gif|jpg|jpeg))', '\\1large\\3', result)
+    result = normalize(result)
+
+    return result
